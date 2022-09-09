f4fc01d82d68b3016763daa37bde8e3d66b0a849
tykoon787 <tykoon787@protonmail.com>
2022-09-09 23:09:53 +0300

    created_6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
