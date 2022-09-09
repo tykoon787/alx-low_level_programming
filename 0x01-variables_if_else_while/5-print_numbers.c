74a9aa2879ab5508673980c9cd5d738ba369ec37
tykoon787 <tykoon787@protonmail.com>
2022-09-09 22:57:26 +0300

    created_5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point 
+ *
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+	printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
