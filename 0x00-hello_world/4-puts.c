fb49989d830d1ed969d07339849c8ab6f04fe5f8
tykoon787 <tykoon787@protonmail.com>
2022-09-09 04:00:40 +0300

    Updated Script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+* Main - Entry Point
+*
+* Return: Always 0 (Success)
+*
+*/
 int main(void)
 {
 char myString[] = "\"Programming is like building a multilingual puzzle";
