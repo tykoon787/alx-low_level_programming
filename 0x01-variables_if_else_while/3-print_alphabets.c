6c019c41f5df99c24de15e5dfdd0d26dcefa97f5
tykoon787 <tykoon787@protonmail.com>
2022-09-09 22:27:15 +0300

    created_3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	putchar(tolower(x));
+	putchar(toupper(x));
+	}
+	putchar('\n');
+	return (0);
+}
