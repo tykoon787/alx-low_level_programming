32e6dc0ee23d0bf7986868b39e32cc5725b76e7a
tykoon787 <tykoon787@protonmail.com>
2022-09-13 17:54:20 +0300

    Updated code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * main - Entry Point
+ * print_alphabet - Prints all the alphabets in lower case
  *
- * Return: 0 (Success) 
+ * Return: Nothing
  *
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		_putchar(lower);
+	}
+	_putchar('\n');
 }
