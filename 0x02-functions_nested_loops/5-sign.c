38bf19541915961dbe6d9ad9bcec5c854aac3d35
tykoon787 <tykoon787@protonmail.com>
2022-09-13 19:03:02 +0300

    created_5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks the sign of a number 
+ * @n: Number to be checked 
+ * Return: 1 if > 0; 0 if == 0; -1 if < 0.
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
