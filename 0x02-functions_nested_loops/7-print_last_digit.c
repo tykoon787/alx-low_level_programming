0ac69a9764bb940448a2d6bf0812dbfa38d19a82
tykoon787 <tykoon787@protonmail.com>
2022-09-13 20:16:51 +0300

    created_7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: Number to be checked
+ * Return: Last digit
+ *
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
