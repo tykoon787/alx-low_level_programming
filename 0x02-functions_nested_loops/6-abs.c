ee0ab0481436b8b591e6a67e12501379808d18d4
tykoon787 <tykoon787@protonmail.com>
2022-09-13 19:20:16 +0300

    created_6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Returns the absolute value of any number
+ * @n: Value to be checked
+ * Return: 0 (Success)
+ */
+int _abs(n)
+{
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		_putchar((n - n)+0);
+	}
+	return (0);
+}
