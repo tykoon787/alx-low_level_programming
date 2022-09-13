27813efa66d39aac4f70f8242892dc0a4718486d
tykoon787 <tykoon787@protonmail.com>
2022-09-13 23:07:43 +0300

    created_10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two intergers
+ * @a: First Interger
+ * @b: Second Integer
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
