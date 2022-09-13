fe98e297f8ab938d3575fb28cbedd5da69dd186d
Oduor Otieno <tykoon787@protonmail.com>
2022-09-13 16:47:03 +0300

    Updated Header File

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,11 @@
 char _putchar(char c);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char lower; 
+
+	for (lower = 'a'; lower <= 'z'; lower ++)
+	{
+		_putchar(lower);
+	}
+	_putchar('\n');
+};
