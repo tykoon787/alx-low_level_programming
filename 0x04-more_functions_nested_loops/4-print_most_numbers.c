a02ed87c004f058696b8e2f4f4ec6cdd2e397304
tykoon787 <tykoon787@protonmail.com>
2022-09-16 02:19:56 +0300

    created_4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0 - 9 excep for 2 and 4
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}
