c45bcd22c937dfc3328e0307e344c1813aa2810b
tykoon787 <tykoon787@protonmail.com>
2022-09-16 02:23:54 +0300

    Created Code

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14
+ * 
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i, j = 0;
+
+	for (; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
