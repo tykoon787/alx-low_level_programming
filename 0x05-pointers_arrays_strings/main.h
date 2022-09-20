a685f0d0b7eb1d345410d9740e9c41fc486fe4a3
tykoon787 <tykoon787@protonmail.com>
2022-09-20 05:04:47 +0300

    Updated Header File

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ void _puts(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *s);
-void puts_half(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif /* MAIN_H */
