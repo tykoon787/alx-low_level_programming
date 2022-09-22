52979669e0ea4b545345f5a1f6afff5246beb757
tykoon787 <tykoon787@protonmail.com>
2022-09-22 21:33:20 +0300

    Created Backup code

---
+++ alxCatchUp/0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _strncat- Copies n bytes from src to dest
+ * @dest: Destination
+ * @src: Source
+ * @n: number of bytes to append to dest
+ * Return: start
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+	char *start = dest;
+	char *temp = src;
+
+	while (*dest != '\0')
+	{
+	dest++;
+	}
+
+	int src_len = 0;
+
+	while (*src != '\0')
+	{
+	src_len++;
+	src++;
+	}
+
+	if (n > src_len)
+	{
+		n = src_len;
+	}
+	else
+	{
+		n = n;
+	}
+
+	src = temp;
+
+	while (n != 0)
+	{
+	*dest = *src;
+	dest++;
+	src++;
+	n--;
+	}
+
+	*dest = '\0';
+	return (start);
+}
