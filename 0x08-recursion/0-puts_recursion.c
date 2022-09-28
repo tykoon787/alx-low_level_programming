0c308c3415d6ba95856feb79977d622af1e8fe24
tykoon787 <tykoon787@protonmail.com>
2022-09-28 13:24:35 +0300

    Created Code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- Function that prints a string
+ * @s: Input String
+ * Return: Nothing
+ *
+ */
+void _puts_recursion(char *s)
+{
+
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	_putchar(s[i]);
+	i++;
+	}
+	_putchar('\n');
+}
