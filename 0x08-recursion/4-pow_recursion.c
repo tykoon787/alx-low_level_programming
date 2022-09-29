1bde4b4bfb131a927a278f41efb6175d7eeabbc0
tykoon787 <tykoon787@protonmail.com>
2022-09-29 04:39:19 +0300

    Created Code

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion- Used to calculate x ^ y
+ * @x: Base
+ * @y: Exponent
+ * Return: x ^ y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	return (x *= _pow_recursion(x, y - 1));
+}
