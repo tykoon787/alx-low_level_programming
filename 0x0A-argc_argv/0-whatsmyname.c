3728a4ae1cab6bf6346fbe180cf460788fae63ea
tykoon787 <tykoon787@protonmail.com>
2022-10-01 02:31:43 +0300

    Created Code

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 Success
+ *
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+
+	return (0);
+}
