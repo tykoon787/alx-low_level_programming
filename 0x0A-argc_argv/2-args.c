b40f8c3a3c40c8ba756423c3bbccda6c1f59a086
tykoon787 <tykoon787@protonmail.com>
2022-10-01 03:05:16 +0300

    Created Code

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- Entry Point
+ * @argc: Argument Count
+ * @argv: Argument Vector
+ * Return: (0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i =1; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+	return (0);
+}
