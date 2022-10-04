aaec6dc80d2ccdf92172fabdac41c28840282344
tykoon787 <tykoon787@protonmail.com>
2022-10-04 21:06:09 +0300

    Created Code

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - A Function that frees a 2D array of ints previously
+ * created by your malloc_grid function
+ * @grid: Input 2D array to be freed
+ * @height: Height of Grid 
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid == NULL)
+		return;
+	while (i < height)
+		free((int *)grid[i++]);
+	free(grid);
+}
