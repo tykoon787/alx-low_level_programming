8a18f087fa88080e75408a5d8edca56cf2401ceb
tykoon787 <tykoon787@protonmail.com>
2022-10-12 23:09:38 +0300

    Created Code

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - This is just a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H*/
