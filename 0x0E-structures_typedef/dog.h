52356eaed60e181b84cdb5b98975bfd03823f0ce
tykoon787 <tykoon787@protonmail.com>
2022-10-13 01:12:16 +0300

    Updated code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,15 @@
  * @owner: Owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H*/
