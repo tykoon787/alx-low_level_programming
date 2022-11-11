7038805383492945aa84500c2bbb5ffd6326a716
tykoon787 <tykoon787@protonmail.com>
2022-11-11 12:24:27 +0300

    Header File

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,18 +7,4 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
-/* Functions */
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 #endif /* MAIN_H */
