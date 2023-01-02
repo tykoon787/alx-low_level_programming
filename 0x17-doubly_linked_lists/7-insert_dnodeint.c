3a52ebcc77bc8cffe71bc97423d730c268b2022f
Oduor Otieno <tykoon787@protonmail.com>
2023-01-02 14:13:55 +0300

    Updated code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,69 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - A function that inserts a node at
- * position in a list.
- * @h: The double pointer to the head.
- * @idx: The index to insert new node at.
- * @n: The data to add to new node.
- * Return: A pointer to new element, or NULL on failure.
+ * getlen - gets length of list
+ * @h: pointer to head node
+ * Return: length of node
  */
+
+size_t getlen(dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (i);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+/**
+ * insert_dnodeint_at_index - inserts node at specified index
+ * @h: double pointer to head node
+ * @idx: index
+ * @n: data value
+ * Return: address of new node
+ */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *temp = NULL;
-	unsigned int i = 0;
+	dlistint_t *temp = *h;
+	dlistint_t *prevNode, *newNode;
+	size_t len = getlen(temp);
+	size_t i = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	if (idx > len)
 		return (NULL);
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-	if (!h || !(*h))
-		*h = new_node;
-	else
+
+	if (idx == 0)
 	{
-		temp = *h;
-		while (idx != i++ && temp->next)
-			temp = temp->next;
-		if (temp->next)
-			new_node->prev = temp->prev;
-		else
-			new_node->prev = temp;
-		if (idx == i)
-			temp->next = new_node, new_node->prev = temp;
-		else if (idx == i - 1)
-		{
-			if (temp->prev)
-				temp->prev->next = new_node;
-			else
-				*h = new_node;
-			temp->prev = new_node;
-			new_node->next = temp;
-		}
-		else
-		{
-			free(new_node);
-			return (NULL);
-		}
+		newNode = add_dnodeint(h, n);
+		return (newNode);
 	}
-	return (new_node);
+	if (idx == len)
+	{
+		newNode = add_dnodeint_end(h, n);
+		return (newNode);
+	}
+	while (i < idx)
+	{
+		temp = temp->next;
+		i++;
+	}
+	prevNode = temp->prev;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	prevNode->next = newNode;
+	newNode->prev = prevNode;
+	newNode->next = temp;
+	temp->prev = newNode;
+
+	return (newNode);
+
 }
