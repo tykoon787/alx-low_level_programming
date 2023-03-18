1230aa645752f772cc23c63bab68da2921285a69
tykoon787 <tykoon787@protonmail.com>
2023-03-18 11:06:40 +0300

    Updated code

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+function that returns the perimeter of an island
+represented by a grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    calculates the perimeter of a grid
+    """
+    perimeter = 0
+    nrows = len(grid)
+
+    if grid != []:
+        ncolumns = len(grid[0])
+
+    for a in range(nrows):
+        for b in range(ncolumns):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == nrows or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == ncolumns or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
